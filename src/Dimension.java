0acd47876167e20cb17920e1d71067f4bdf04319
Nicolás Vrancovich <nvrancovich@gmail.com>
2024-05-09 10:34:03 -0300

    Arreglo errores en clase Dimension

--- src/Dimension.java
+++ src/Dimension.java
@@ -1,26 +1,3 @@
-import java.io.IOException;
-import java.util.List;
-
 public class Dimension {
-    String nombre;
-    CSVTable tabla;
 
-    Dimension(String nombreDimension, String rutaArchivoCSV, String columnaId){
-        nombre = nombreDimension;
-        try {
-            tabla = CSVReader.readCSV(rutaArchivoCSV);
-            FormatoTabular tablaTabular = new FormatoTabular(tabla);
-            tablaTabular.printTabularData();
-            System.out.println("");
-            System.out.println("Se cargó la dimensión: " + nombre);
-            List<String> elementos = tabla.getColumns();
-            elementos.remove(columnaId);
-            elementos.remove("");
-            System.out.println("Id: " + columnaId);
-            System.out.println("Elementos: " + elementos);
-        } 
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-}   
+}
