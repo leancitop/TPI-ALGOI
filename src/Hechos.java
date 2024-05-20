206a605091ac586983aab5757292a67bad0c8de2
Nicolás Vrancovich <nvrancovich@gmail.com>
2024-05-20 17:51:00 -0300

    Creo la clase TablaFinal

--- src/Hechos.java
+++ src/Hechos.java
@@ -6,8 +6,11 @@ public class Hechos {
     private Tabla tabla;
     private Map<String, Integer> indexadoColumnas; // Asegúrate de que esta línea esté presente
 
-    // Constructor
+
     public Hechos(String path){
+
+        // TODO tiene que comprobar que existan las 3 dimensiones, o devolver una excepción
+
         this.tabla = LectorArchivos.leerCSV(path);
         this.indexadoColumnas = new HashMap<>(); // Inicializar el mapa
         
@@ -19,8 +22,6 @@ public class Hechos {
         System.out.println("Se cargaron los hechos");
     }
 
-
-    // Getter para indexadoColumnas
     public Map<String, Integer> getIndexadoColumnas() {
         return indexadoColumnas;
     }
@@ -32,22 +33,10 @@ public class Hechos {
         List<FilaTabla> filas = tabla.getFilas();
         for (int i = 0; i < filas.size(); i++) {
             dimensionesMap.put(
-                filas.get(i).getContenido(indexadoColumnas.get(idDimension)), 
-                filas.get(i).getContenido(indexadoColumnas.get(valor)));
+                filas.get(i).getContenidoColumna(indexadoColumnas.get(idDimension)), 
+                filas.get(i).getContenidoColumna(indexadoColumnas.get(valor)));
         }
         return dimensionesMap;
     }
 
-    // Puedes agregar otros métodos aquí según sea necesario
 }
-
-
-
-// id_producto;id_punto_venta;id_fecha;cantidad;valor_unitario;valor_total;costo
-// 235;312;55;2;28.84;57.68;63.45
-// 351;312;55;2;2024.99;4049.98;3796.19
-// 348;312;55;2;2024.99;4049.98;3796.19
-
-// - tiene que comprobar que existan las 3 dimensiones, o devolver una excepción
-
-// - tiene que tener un GET que devuelva a partir de la tabla hechos armar un mapa con id_dimension : id_valor
---
+++ src/TablaFinal.java
@@ -0,0 +1,13 @@
+public class TablaFinal {
+    
+// tiene que guardar en una tabla el resultado del cruce entre el dict 
+// que genera la dimension y el valor y la medida que se eligio
+
+    TablaFinal(){};
+
+    void imprimirTablaFinal(String dimension, String valor, String medida){
+        // metodo que toma los maps e imprime en formato tabular la tabla final
+    }
+
+S
+}
