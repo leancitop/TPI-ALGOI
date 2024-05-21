7831d69bf24c175130039cb1084530f3691d7209
leancito <lean.3scudero@gmail.com>
2024-05-21 12:53:31 -0300

    Incremento en clase Dimension

--- src/Dimension.java
+++ src/Dimension.java
@@ -5,11 +5,19 @@ import java.util.Map;
 
 public class Dimension {
     private String nombre;
-    private Map<String, List<String>> valores;
+    private String col_id;
+    private Map<String, List<String>> valores;    // el map valores : hechos_id
+    private Map<String, List<String>> valores_id; // el map valores : dim_id
+    private Tabla tabla_dimension;
 
-    public Dimension(String n){
+    public Dimension(String n, String i){
         this.nombre = n;
         this.valores = new HashMap<>();
+        this.col_id = i;
+    }
+
+    public void setTabla(Tabla t){
+        this.tabla_dimension = t;
     }
 
     public void agregarValor(String valorCSV, String idFila) { // Si aun no existe el valor, arma la lista de IDS. Si ya existe, le suma el id a la lista existente de ese valor
@@ -19,6 +27,11 @@ public class Dimension {
     public String getNombre() {
         return nombre;
     }
+
+    public String getId() {
+        return col_id;
+    }
+
 
     public Map<String, List<String>> getValores() {
         return valores;
