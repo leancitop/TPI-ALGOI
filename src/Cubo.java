1d5b0af13f4fa34430a8402fdef4115342f56547
Valen <vbustossak@estudiantes.unsam.edu.ar>
2024-05-24 17:29:40 -0300

    Clase Cubo metodos base. Falta definirlos.

--- src/Cubo.java
+++ src/Cubo.java
@@ -3,33 +3,64 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-
 public class Cubo {
-    String nombre;
-    int cantidadDimensiones;
-    Map<String, Dimension> listaDimensiones;
-    // Por cada dimensión:
-    //  - Niveles jerárquicos
-    //  - Miembros (elementos) en formato tabular
-    Map<String, Hechos> listaHechos;
-    // Medidas representadas en cada celda
-    private int[] columnasHechos;   // indices de columnas numericas (hechos) en la tabla de hechos
+    private String nombre;
+    private List<Dimension> dimensionesCargadas;
+    private List<Integer> dimensionesElegidas;
+    private Hechos hechosCargados;
+    private List<Integer> valores;
+    private List<Medida> medidas; 
 
-    Cubo(String nombreCubo){
-        nombre = nombreCubo;
-        listaDimensiones = new HashMap<String, Dimension>();
+
+    private Cubo(String nombre){
+        this.nombre = nombre;
+        List<Dimension> dimensiones = new ArrayList<>();
+        List<Dimension> dimensionesElegidas = new ArrayList<>();
     }
 
-    String getNombre(){
-        return nombre;
+    public void crearCubo(String nombre){
+        Cubo cubo = new Cubo(nombre);
     }
 
-    Map<String, Dimension> getDimensiones(){
-        return listaDimensiones;
+    void agregarDimensiones(List<Dimension> dimensiones){
+        for (Dimension dimension : dimensiones) {
+            this.dimensionesCargadas.add(dimension);
+        }
     }
 
-    void agregarDimension(Dimension dimension){
-        listaDimensiones.put(dimension.getNombre(), dimension);;
+    public void agregarHecho(Hechos hechos){ // Dimensiones debe coincidir con ID en hechos. Todo cargado, mostramos resumen
+        this.hechosCargados = hechos;        
     }
 
+    public void elegirDimensiones(List<Integer> dimensiones){ //Se elige dimensiones que vamos a mostrar de todas las cargadas.
+        this.dimensionesElegidas = dimensiones;
+    }
+
+    public void elegirValores(List<Integer> valores){ //Valores que tenga la tabla de hechos. Se le puede pasar solo un valor
+        this.valores = valores;
+    } 
+
+    public void elegirMedidas(List<Medida> medidas){ 
+        this.medidas = medidas;
+    }
+
+    public void rollUp(){
+
+    }
+
+    public void drillDown(){
+
+    }
+
+    public void slice(){
+
+    }
+
+    public void dice(){
+
+    }
+
+    public void proyectar(){
+
+    }
 }
