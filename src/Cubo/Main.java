280dfe92f18092d090ec6a40aa9fc15e9d4a7513
leancito <lean.3scudero@gmail.com>
2024-05-29 20:32:14 -0300

    comenté los resultados de los test

--- src/Cubo/Main.java
+++ src/Cubo/Main.java
@@ -5,8 +5,8 @@ import Test.TestParsear;
 
 public class Main {
     public static void main(String[] args) {
-        TestAgrupar test = new TestAgrupar();
+        TestAgrupar test = new TestAgrupar(); // devuelve una tabla, pero no está agrupada correctamente
         System.out.println("------------------------------");
-        TestParsear test2 = new TestParsear();
+        TestParsear test2 = new TestParsear(); // devuelve una tabla vacía, solo están los headers
     }
 }
