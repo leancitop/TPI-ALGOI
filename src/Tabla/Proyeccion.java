9a5dfee07f7e997d6fad137d62ebc929bc6dd487
Valentino <vbustossak@estudiantes.unsam.edu.ar>
2024-06-07 07:40:55 -0300

    proyeccion.imprimirNfilas

--- src/Tabla/Proyeccion.java
+++ src/Tabla/Proyeccion.java
@@ -52,6 +52,50 @@ public void imprimirPrimerasDiezFilas() {
         }
         System.out.println();
     }
+}   
+public void imprimirNfilas(int nfilas) {
+    // Obtener las primeras diez filas de la tabla
+    int numeroFilas = Math.min(tabla.getNumeroFilas(), nfilas);
+
+    // Obtener encabezados y columnas
+    String[] headers = tabla.getHeaders();
+    List<Columna<?>> columnas = tabla.getColumnas();
+
+    // Determinar el ancho máximo de cada columna
+    int[] anchosMaximos = new int[headers.length];
+    for (int i = 0; i < headers.length; i++) {
+        anchosMaximos[i] = headers[i].length();
+    }
+    for (int i = 0; i < numeroFilas; i++) {
+        for (int j = 0; j < columnas.size(); j++) {
+            Object valor = columnas.get(j).getContenidoFila(i);
+            int longitud = valor.toString().length();
+            if (longitud > anchosMaximos[j]) {
+                anchosMaximos[j] = longitud;
+            }
+        }
+    }
+
+    // Imprimir encabezados
+    for (int i = 0; i < headers.length; i++) {
+        System.out.print(String.format("%-" + anchosMaximos[i] + "s", headers[i]) + " ");
+    }
+    System.out.println();
+
+    // Imprimir separadores
+    for (int ancho : anchosMaximos) {
+        System.out.print(String.format("%-" + ancho + "s", "").replace(' ', '-') + " ");
+    }
+    System.out.println();
+
+    // Imprimir datos
+    for (int i = 0; i < numeroFilas; i++) {
+        for (int j = 0; j < columnas.size(); j++) {
+            Object valor = columnas.get(j).getContenidoFila(i);
+            System.out.print(String.format("%-" + anchosMaximos[j] + "s", valor) + " ");
+        }
+        System.out.println();
+    }
 }
 
 
